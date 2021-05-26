099737cafd225e7de6f21730d927a5b6f46b0605
Shawn <shawntimmel@gmail.com>
2021-05-27 02:30:32 +0800

    lab2

--- Lab2-20210309/lab2_task1_glass_binary.py
+++ Lab2-20210309/lab2_task1_glass_binary.py
@@ -9,21 +9,22 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn.functional as F
+from matplotlib import pyplot as plt
 
 """
 Step 1: Load data and pre-process data
 """
 # load all data
 data = pd.read_csv('glass.csv',  header=None)
-
-
-
+# drop first column as it is identifier
 data.drop(data.columns[0], axis=1, inplace=True)
 
 # try shuffle data
 data = data.sample(frac=1).reset_index(drop=True)
 
-
+# split data into 2 classes to perform binary classification
+# by replacing class values in the range of 1-4 (window glass)
+# with 1, and 5-7 (non-window glass) with 0
 data[data.shape[1]] = (data[data.shape[1]] < 5).astype(int)
 
 # randomly split data into training set (80%) and testing set (20%)
@@ -38,17 +39,26 @@ n_features = train_data.shape[1] - 1
 train_input = train_data.iloc[:, :n_features]
 train_target = train_data.iloc[:, n_features]
 
+# normalise training data by columns
+#for column in train_input:
+#    train_input[column] = train_input.loc[:, [column]].apply(lambda x: (x - x.min()) / (x.max()
+#    - x.min()))
+    # train_input[column] = train_input.loc[:, [column]].apply(lambda x: (x - x.mean()) / x.std())
+
 # split training data into input and target
 # the first 9 columns are features, the last one is target
 test_input = test_data.iloc[:, :n_features]
 test_target = test_data.iloc[:, n_features]
 
-
+# normalise testing input data by columns
+#for column in test_input:
+#    test_input[column] = test_input.loc[:, [column]].apply(lambda x: (x - x.min()) / (x.max() -
+#x.min()))
+    # test_input[column] = test_input.loc[:, [column]].apply(lambda x: (x - x.mean()) / x.std())
 
 # create Tensors to hold inputs and outputs
 X = torch.Tensor(train_input.values).float()
 Y = torch.Tensor(train_target.values).long()
-
 
 """
 Step 2: Define a neural network 
@@ -65,28 +75,47 @@ based on the computed gradients.
 Its performance will be evaluated using cross-entropy.
 """
 
-#TODO define the number of inputs, classes, training epochs, and learning rate
+# define the number of inputs, classes, training epochs, and learning rate
+input_neurons = n_features
+hidden_neurons = 10
+output_neurons = 2
+learning_rate = 0.01
+num_epochs = 500
 
 
+# define a customised neural network structure
+class TwoLayerNet(torch.nn.Module):
 
+    def __init__(self, n_input, n_hidden, n_output):
+        super(TwoLayerNet, self).__init__()
+        # define linear hidden layer output
+        self.hidden = torch.nn.Linear(n_input, n_hidden)
+        # define linear output layer output
+        self.out = torch.nn.Linear(n_hidden, n_output)
 
-#TODO define a customised neural network structure
+    def forward(self, x):
+        """
+            In the forward function we define the process of performing
+            forward pass, that is to accept a Variable of input
+            data, x, and return a Variable of output data, y_pred.
+        """
+        # get hidden layer input
+        h_input = self.hidden(x)
+        # define activation function for hidden layer
+        h_output = torch.sigmoid(h_input)
+        # get output layer output
+        y_pred = self.out(h_output)
 
+        return y_pred
 
+# define a neural network using the customised structure
+net = TwoLayerNet(input_neurons, hidden_neurons, output_neurons)
 
+# define loss function
+loss_func = torch.nn.CrossEntropyLoss()
 
-#TODO define a neural network using the customised structure 
-
-
-
-#TODO define loss function (https://pytorch.org/docs/stable/nn.html#loss-functions)
-
-
-
-#TODO define optimiser (https://pytorch.org/docs/stable/optim.html)
-
-
-
+# define optimiser
+optimiser = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
 # store all losses for visualisation
 all_losses = []
@@ -127,9 +156,9 @@ for epoch in range(num_epochs):
 
 # import matplotlib.pyplot as plt
 #
-# plt.figure()
-# plt.plot(all_losses)
-# plt.show()
+plt.figure()
+plt.plot(all_losses)
+plt.show()
 
 """
 Evaluating the Results
